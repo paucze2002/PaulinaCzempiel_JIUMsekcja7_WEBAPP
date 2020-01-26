f63e250c33124aa19557b1a1581263fd4a3e5aa0
paucze2002 <32535447+paucze2002@users.noreply.github.com>
2020-01-26 16:29:38 +0100

    Add files via upload

---
+++ Raport3/MathAppWeb/src/java/mathapplication/servlet/Second.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathapplication.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *  Second servlet which gets numbers.
+ * @author Paulina Czempiel
+ * @version 1.0
+ */
+@WebServlet(name = "Second", urlPatterns = {"/Second"})
+public class Second extends HttpServlet {
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+	
+		
+        out.println("<html>\n<body>\n<h1>History</h1>\n");
+		
+		
+        
+        HttpSession session = request.getSession();
+        Date time = new Date(session.getLastAccessedTime());
+			
+        out.println("<br> Last time visited: " + time);
+		 
+        Cookie[] cookies = request.getCookies();
+        String timesVisited = "1";
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("timesVisited")) {
+                timesVisited = cookie.getValue();
+                break;
+            }
+        }
+      
+      out.println("<br> <br> Times Visited this site: " + timesVisited);
+      out.println("</body>\n</html>");
+		 
+      int visits = Integer.parseInt(timesVisited);
+      visits++;
+		 
+      Cookie cookie = new Cookie("timesVisited", Integer.toString(visits));
+      response.addCookie(cookie);
+		
+	}
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Raport3/MathAppWeb/test/mathapplication/model/MathModelTest.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathapplication.model;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+import mathapplication.exception.MathNoDataException;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+
+/**
+ *  Class test MathModel - method count
+ * 
+ * @author Paulina Czempiel
+ * @version 1.0
+ */ 
+@SuppressWarnings("unused")
+public class MathModelTest {
+
+    /**
+     * Tested object
+     */
+    MathModel mathmodel;
+    
+    /**
+     * Creates new model before every test
+     */
+    @Before
+    public void setup(){
+        mathmodel = new MathModel();
+        mathmodel.clearResults();
+        mathmodel.clearArray();
+    }
+    
+
+    /**
+     * Test of count method, of class MathModel.
+     * Test null array - no data to count.
+     */
+    @Test
+    public void testCountInputNoData() {
+        System.out.println("No input data test" );
+        try{
+            mathmodel.count();
+            fail("Incorrect input! An exception should have benn thrown");
+        } 
+        catch(NullPointerException | MathNoDataException e){
+            
+        }
+    }
+    
+     /**
+     * Test of count method, of class MathModel.
+     * Test correct input. Median.
+     */
+    @Test
+    public void testCountCorrectDataMedian() {
+        System.out.println("Correct input test." );
+        try{
+            String tmp = "3.0 3.0 3.0";
+            mathmodel.setNumbers(tmp);
+            mathmodel.count();
+            assertEquals("Median should have been 3.", 3.0, mathmodel.getMedian(), 0);
+        }
+        catch(NullPointerException | MathNoDataException e){
+            fail("Counting fails.");
+        }
+    }
+    
+      /**
+     * Test of count method, of class MathModel.
+     * Test correct input. Average
+     */
+    @Test
+    public void testCountCorrectDataAverage() {
+        System.out.println("Correct input test." );
+        try{
+            String tmp = "3.0 3.0 3.0";
+            mathmodel.setNumbers(tmp);
+            mathmodel.count();
+            assertEquals("Average should have been 3", 3.0, mathmodel.getAverage(), 0);
+        }
+        catch(NullPointerException | MathNoDataException e){
+            fail("Counting fails.");
+        }
+    }
+    
+     /**
+     * Test of count method, of class MathModel.
+     * Test correct input. Standard Deviation.
+     */
+    @Test
+    public void testCountCorrectDataStandardDeviation() {
+        System.out.println("Correct input test." );
+        try{
+            String tmp = "3.0 3.0 3.0";
+            mathmodel.setNumbers(tmp);
+            mathmodel.count();
+            assertEquals("Median should have been 0.", 0.0, mathmodel.getStandardDeviation(), 0);
+        }
+        catch(NullPointerException | MathNoDataException e){
+            fail("Counting fails.");
+        }
+    }
+    
+    
+}
+
+
